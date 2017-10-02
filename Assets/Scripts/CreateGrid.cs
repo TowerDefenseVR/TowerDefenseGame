fefaa13c1f9996773118e4a5e3259d4080dd69aa
Håkon Paulsen <haakonpaulsen1234@hotmail.com>
2017-10-02 22:57:46 +0200

    work in progress
    load image and reads pixel color

--- Assets/Scripts/CreateGrid.cs
+++ Assets/Scripts/CreateGrid.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class CreateGrid : MonoBehaviour {
@@ -10,6 +11,20 @@ public class CreateGrid : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		createMap();
+		createMapFromImg("C:/Users/wizard man/Documents/GitHub/TowerDefenseVR/Assets/maps/map_test.png");		
+	}
+
+	public static Texture2D LoadPNG(string filePath) {
+		Texture2D tex = null;
+		byte[] fileData;
+
+		if (File.Exists(filePath))     {
+			print("loaded image");
+			fileData = File.ReadAllBytes(filePath);
+			tex = new Texture2D(1,1);
+			tex.LoadImage(fileData);
+		}
+		return tex;
 	}
 
 	void createMap() {
@@ -21,6 +36,22 @@ public class CreateGrid : MonoBehaviour {
 				tile.transform.position = new Vector3(i*(10+spaceBetween), 0, j*(10+spaceBetween));
 				tiles.Add(tile);
 			}
+		}
+	}
+
+	void createMapFromImg(string path) {
+		Texture2D tex = LoadPNG(path);
+		Color pixel_colour = tex.GetPixel(4,1);
+		if(pixel_colour == Color.black) {
+			print(pixel_colour + " => path");
+		} else if(pixel_colour == Color.green) {
+			print(pixel_colour + " => ground");
+		} else if(pixel_colour == Color.blue) {
+			print(pixel_colour + " => start");
+		} else if(pixel_colour == Color.red) {
+			print(pixel_colour + " => end");
+		} else {
+			print(pixel_colour + " => unknown");
 		}
 	}
 	
