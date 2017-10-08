a997d218b49e974e965e1b889d306f7bb77e6563
Håkon Paulsen <haakonpaulsen1234@hotmail.com>
2017-10-08 23:40:45 +0200

    rotate to nearest enemy and fire within radius
    still got to destroy the bullet object heh, enough for 1 day

---
+++ Assets/Scripts/TowerFire.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFire : MonoBehaviour {
+	public Transform bulletPrefab;
+	public float fireRate;
+	public float range;
+	private Transform target;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		target = FindNearestEnemy();
+		if(target != null) {
+			RotateTowardEnemy();
+			if(Vector3.Distance(transform.position, target.position) <= range && !IsInvoking("Fire")) {
+				print("FIRE!");
+				InvokeRepeating("Fire", 0.0f, 1/fireRate);
+			} else if(Vector3.Distance(transform.position, target.position) > range && IsInvoking("Fire")) {
+				print("HALT FIRE!");
+				CancelInvoke("Fire");
+			}
+		}
+	}
+
+	void RotateTowardEnemy() {
+		Vector3 targetDir = target.position - transform.position;
+        float step = 10 * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+        transform.rotation = Quaternion.LookRotation(newDir);
+		transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+	}
+
+	Transform FindNearestEnemy() {
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		Transform closest = null;
+		float dist = Mathf.Infinity;
+        Vector3 pos = transform.position;
+		foreach(GameObject enemy in enemies) {
+			Vector3 diff = enemy.transform.position - pos;
+            float curDist = diff.sqrMagnitude;
+            if (curDist < dist) {
+                closest = enemy.transform;
+                dist = curDist;
+            }
+        }
+        return closest;
+	}
+
+	void Fire() {
+		bulletPrefab = Instantiate(bulletPrefab, transform.position, transform.rotation);
+		bulletPrefab.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3(0, 0, 50));
+	}
+}
--- Assets/Scripts/TowerPlacement.cs
+++ Assets/Scripts/TowerPlacement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class TowerPlacement : MonoBehaviour {
 	public Transform towerPrefab;
+	public float radiusBetween;
 
 
 	// Use this for initialization
 	void Start () {
+		towerPrefab = Instantiate(towerPrefab, towerPrefab.position, towerPrefab.rotation, GameObject.Find("Towers").transform);
 	}
 	
 	// Update is called once per frame
@@ -16,12 +18,11 @@ public class TowerPlacement : MonoBehaviour {
 		RaycastHit hit;
 		// Casts the ray and get the first game object hit
 		Physics.Raycast(ray, out hit);
-
 		towerPrefab.position = new Vector3(hit.point.x, (hit.point.y+towerPrefab.lossyScale.y/2), hit.point.z);
-
-		if(Input.GetButtonDown("Fire1")){
+		if(Input.GetButtonDown("Fire1") && Physics.OverlapSphere(towerPrefab.position, radiusBetween, 1 << LayerMask.NameToLayer("Ignore Raycast")).Length <= 1){
 			Instantiate(towerPrefab, towerPrefab.position, towerPrefab.rotation, GameObject.Find("Towers").transform);
+		} else if(Input.GetButtonDown("Fire1") && Physics.OverlapSphere(towerPrefab.position, radiusBetween, 1 << LayerMask.NameToLayer("Ignore Raycast")).Length > 1) {
+			print("TOO CLOSE TO ANOTHER TOWER");
 		}
-
     }
 }
