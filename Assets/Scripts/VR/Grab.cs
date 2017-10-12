426084b404091aa5a2770eccfcfb0a24439dad04
Håkon Paulsen <haakonpaulsen1234@hotmail.com>
2017-10-12 19:49:04 +0200

    Added VR support!

---
+++ Assets/Scripts/VR/Grab.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour {
+
+	bool isHolding = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerStay(Collider c) {
+		if (VRController.IsTrigged && !isHolding) {
+			print("Holding " + c.gameObject.name);
+			c.gameObject.transform.parent = gameObject.transform;
+			c.gameObject.GetComponent<Rigidbody>().useGravity = false;
+			c.gameObject.GetComponent<BoxCollider>().enabled = false;
+			isHolding = true;
+		}
+		print("fuck");
+	}
+}
---
+++ Assets/Scripts/VR/VRController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class VRController : MonoBehaviour {
+
+	private SteamVR_TrackedObject trackedObject;
+	private SteamVR_Controller.Device controller {
+         get { return SteamVR_Controller.Input((int)trackedObject.index); }
+    }
+
+
+	public static bool IsTrigged = false;
+
+	// Use this for initialization
+	void Start () {
+		trackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( controller.GetHairTrigger()){
+            IsTrigged = true;
+        } else {
+			IsTrigged = false;
+		}
+	}
+
+}
